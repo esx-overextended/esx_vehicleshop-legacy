d622783738102e4a6bf50a91f5f5cd0cfc3548b4
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-07-19 22:49:02 -0400

    refactor: initial utilization of ESX.CreateVehicle on buy

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,20 +5,19 @@ description 'ESX Vehicle Shop'
 version '1.0'
 legacyversion '1.9.1'
 
-shared_script '@es_extended/imports.lua'
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	"config.lua"
+}
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
 	'server/*.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
 	'client/*.lua'
 }
 
